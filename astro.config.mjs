c33cc457ab1bd7a38d2ceef85451c391a47332cc
Pycnocline <6612247@gmail.com>
2024-12-03 14:40:57 +0800

    更改域名配置为Pycnocline.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // 页面设置
 export default defineConfig({
-    site: 'https://iamin.icu',    // 配置自定义域名
+    site: 'https://Pycnocline.github.io',    // 配置自定义域名
 });
